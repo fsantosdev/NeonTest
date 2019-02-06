27c1a2d2319fbfa5260fca271de7c972cc61f5e9
Luiz Filipe Santos da Silva <filipe.santos_@live.com>
2019-02-06 16:03:00 -0200

    ADD Interface Listagem Moedas

--- NeonTest/wwwroot/js/site.js
+++ NeonTest/wwwroot/js/site.js
@@ -1,4 +1,62 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.ENV = null;
+window.currencies = [];
 
-// Write your JavaScript code.
+$(function () {
+    window.ENV = getENV();
+    refreshList();
+
+    $('#refresher').click(() => {
+        refreshList();
+    });
+});
+
+function refreshList() {
+    requestFactory(
+        'moedas/listagem',
+        'GET',
+        null,
+        (response) => {
+            console.info('EXECUTED CALLBACK');
+            window.currencies = response.data;
+
+            let optionsBuild = '';
+            response.data.forEach(item => {
+                optionsBuild += `<option value="${item.sigla}">${item.sigla} - ${item.nome}</option>`;
+            });
+
+            $('#moedaOrigem').append(optionsBuild);
+            $('#moedaDestino').append(optionsBuild);
+
+            optionsBuild = null;
+        }
+    );
+}
+
+function requestFactory(_url, _method, _params, _callback) {
+    console.info(`${window.ENV.BASE_URL}${_url}`);
+
+    $.ajax({
+        url: `${ENV.BASE_URL}${_url}`,
+        method: _method,
+        data: _params
+    })
+    .then(response => {
+        if (_callback)
+            _callback(response);
+    })
+    .always(() => {
+        console.info('ENDED');
+    });
+}
+
+function getENV() {
+    if (window.location.href.indexOf('localhost') > -1) {
+        return {
+            "BASE_URL": "http://localhost:58204/"
+        }
+    } else {
+        return {
+            "BASE_URL": ""
+        }
+    }
+}
